ebb759451d8080acf6d6500088e5b8d4d0a1f129
jakeja04 <jakejallen04@gmail.com>
2022-08-24 18:32:44 -0400

    Initial commit

---
+++ js/searchBar.js
@@ -0,0 +1,12 @@
+let searchBox = document.querySelector('#searchbar');
+let pictures = document.querySelectorAll('a');
+
+searchBox.addEventListener("keyup", (event) => {
+  for(let i = 0; i < pictures.length; i++) {
+    if(pictures[i].dataset.caption.includes(searchBox.value.toLowerCase()) || searchBox.value.length == 0) {
+      pictures[i].style.display = "inline";
+    } else {
+      pictures[i].style.display = 'none';
+    }
+  }
+});
